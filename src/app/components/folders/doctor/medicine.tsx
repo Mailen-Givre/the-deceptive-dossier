4261ea7f810905df22bf011af424aabdbcd8419c
Mai <mailen.givre@gmail.com>
2024-01-29 13:11:35 -0300

    iluminame and medicine text

--- src/app/components/folders/doctor/medicine.tsx
+++ src/app/components/folders/doctor/medicine.tsx
@@ -8,10 +8,10 @@ export default function Medicine() {
             <div className={styles.medicine}>
                 <p>Debo chequear si los medicamentos son los que corresponden o hubo sobredosis. Esta es la receta:</p>
                 <ul>
-                    <li><b>Medalazina:</b> los días de 2 sílabas.</li>
-                    <li><b>Atervastat¡ni:</b> desde el lunes, inclusive, cada 2 días.</li>
-                    <li><b>Onalapril:</b> los días siguientes a aquellos días que llevan una &quot;N&quot; en el nombre.</li>
-                    <li><b>Lanoprost:</b> los días anteriores a aquellos que tienen alguna letra repetida.</li>
+                    <li><b>Medalazina:</b>  1 pastilla los días de 2 sílabas.</li>
+                    <li><b>Atervastat¡ni:</b> 1 pastilla desde el lunes, inclusive, cada 2 días.</li>
+                    <li><b>Onalapril:</b> 1 pastilla los días siguientes a aquellos días que llevan una &quot;N&quot; en el nombre.</li>
+                    <li><b>Lanoprost:</b> 1 pastilla los días anteriores a aquellos que tienen alguna letra repetida.</li>
                 </ul>
             </div>
         </>
